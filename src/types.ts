2de8967e42aa8fc9fcffd75c4beae9de94843676
RevainIDN <kov.igor94@gmail.com>
2025-01-11 19:41:25 +0200

    Добавлен новый интерфейс объекта

--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,39 @@
 export interface MealCategories {
-	idCategory: string,
-	strCategory: string,
-	strCategoryDescription: string,
-	strCategoryThumb: string,
+	idCategory: string;
+	strCategory: string;
+	strCategoryDescription: string;
+	strCategoryThumb: string;
 }
 
 export interface MealCategory {
-	categories: MealCategories[],
+	categories: MealCategories[];
 }
 
 export interface FoodByCategories {
-	strMeal: string,
-	strMealThumb: string,
-	idMeal: string,
+	strMeal: string;
+	strMealThumb: string;
+	idMeal: string;
 }
 
 export interface FoodByCategory {
-	meals: FoodByCategories[],
+	meals: FoodByCategories[];
+}
+
+export interface Recipe {
+	dateModified: string | null;
+	idMeal: string;
+	strArea: string | null;
+	strCategory: string | null;
+	strCreativeCommonsConfirmed: string | null;
+	strDrinkAlternate: string | null;
+	strImageSource: string | null;
+	strInstructions: string;
+	strMeal: string;
+	strMealThumb: string;
+	strSource: string | null;
+	strTags: string | null;
+	strYoutube: string | null;
+
+	strIngredients: (string | null)[];
+	strMeasures: (string | null)[];
 }
