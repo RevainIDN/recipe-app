66ee46f276735bd93876f5404a621969b82e990b
RevainIDN <kov.igor94@gmail.com>
2025-01-12 18:23:19 +0200

    Улучшена пагинация списка рецептов

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -36,19 +36,45 @@ export default function Pagination({ currentPage, recipesPerPage, filteredRecipe
 		}
 	}
 
+	const getDisplayedPages = () => {
+		const maxVisiblePages = 5;
+
+		let startPage = currentPage - 2;
+		let endPage = currentPage + 2;
+
+		if (startPage < 1) {
+			startPage = 1;
+			endPage = Math.min(maxVisiblePages, pageNumbers.length);
+		}
+
+		if (endPage > pageNumbers.length) {
+			endPage = pageNumbers.length;
+			startPage = Math.max(pageNumbers.length - maxVisiblePages + 1, 1);
+		}
+
+		const displayedPages = pageNumbers.slice(startPage - 1, endPage);
+
+		return displayedPages;
+	};
+
+	const displayedPages = getDisplayedPages();
+
 	return (
 		<div className='pagination-cont'>
 			<ul className="pagination">
 				<li><button className='page-btn page-btn-prev' onClick={prevPage}><img src={arrow} alt="Prev" /></button></li>
-				{
-					pageNumbers.map(number => (
-						<li className='page-item' key={number}>
-							<button className={`page-btn ${currentPage === number ? `active` : ''}`} onClick={() => paginate(number)}>
-								{number}
-							</button>
-						</li>
-					))
-				}
+
+				{displayedPages.map((number) => (
+					<li className="page-item" key={number}>
+						<button
+							className={`page-btn ${currentPage === number ? 'active' : ''}`}
+							onClick={() => paginate(number)}
+						>
+							{number}
+						</button>
+					</li>
+				))}
+
 				<li><button className='page-btn page-btn-next' onClick={nextPage}><img src={arrow} alt="Next" /></button></li>
 			</ul>
 		</div>
